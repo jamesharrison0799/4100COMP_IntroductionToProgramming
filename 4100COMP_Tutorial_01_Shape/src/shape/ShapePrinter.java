207c012b17496bad257f11946c1d4e9750041dc9
James <James@DESKTOP-9D7NJM1>
2019-09-25 12:01:54 +0100

    Tutorial 1 - Shape

---
+++ 4100COMP_Tutorial_01_Shape/src/shape/ShapePrinter.java
@@ -0,0 +1,15 @@
+package shape;
+
+public class ShapePrinter {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Box\n");
+		System.out.println("*******");
+		System.out.println("*     *");
+		System.out.println("*     *");
+		System.out.println("*******");
+		System.out.println("\nOval\n");
+		System.out.println("    *** \n  *     *\n *       *\n *       *\n  *     *   \n    ***");
+		System.out.println("\nArrow\n\n   *\n  ***\n *****\n   *\n   *\n   *");
+	}
+}
