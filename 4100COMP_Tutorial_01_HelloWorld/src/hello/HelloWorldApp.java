d9d047af71053b527e1503a515437ab11082be19
James <James@DESKTOP-9D7NJM1>
2019-09-25 12:00:28 +0100

    Tutorial 1 - hello world

---
+++ 4100COMP_Tutorial_01_HelloWorld/src/hello/HelloWorldApp.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class HelloWorldApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World!");
+		
+	}
+
+}
