6eeae70abb4272815425418cc0435bb3aabb096e
cmpjharr <cmpjharr@CMPH019.cms.livjm.ac.uk>
2019-09-26 18:02:19 +0100

    Tutorial 2 - Squares and cubes (Task 4)

---
+++ 4100COMP_Tutorial_02_T4_Squares_and_Cubes/src/powers/SquaresAndCubes.java
@@ -0,0 +1,16 @@
+package powers;
+
+public class SquaresAndCubes {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("Number      Square      Cube");	
+		
+		for (int i = 1; i<=10; i++) {
+			System.out.println(i+"           " + i*i +"           "+ i*i*i );
+		}
+		
+	}
+
+}
