8d16db8d106bfb228ff72978bb367f7ba2b1ed7f
James <James@DESKTOP-9D7NJM1>
2019-10-02 16:57:51 +0100

    4100COMP_Tutorial_04_Lab04

---
+++ 4100COMP_Tutorial_04_Lab04/src/outputTest/OutputTest.java
@@ -0,0 +1,15 @@
+package outputTest;
+
+public class OutputTest {
+
+	public static void main(String[] args) {
+		
+		//double click right of line number to add breakpoint
+		System.out.println("Hello and welcome to 4100COMP");
+		System.out.println("This is the introduction to programming module");
+		System.out.println("This line will be continued");
+		System.out.println(" by the following line");
+		
+	}
+
+}
