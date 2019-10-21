05c8eb9d2eb5a2db5ff5446afe2347653ad99839
James <James@DESKTOP-9D7NJM1>
2019-10-21 15:47:18 +0100

    Fixed bug in Custom Rate Menu. Added additional validation to % input.

--- 4100COMP_Coursework1/src/seatDiscount/SeatDiscount.java
+++ 4100COMP_Coursework1/src/seatDiscount/SeatDiscount.java
@@ -17,37 +17,51 @@ public class SeatDiscount {
 		boolean useCustomRate;
 		final int defualtRate = 20;
 		double customRate = 0;
+		boolean invalidInput = false;
 		
 		//Custom Rate Menu
-		while (true) {
-			
-			System.out.println("Specify Custom Discount Rate [Y|N]: ");
-			String customDiscountRate = console.nextLine();
-			
-			// .toUpperCase is used in case the user submits a lower case character.
-			// The OR operator could also be used eg: if (customDiscountRate.equals("Y") || customDiscountRate.equals("y")){}
-			if (customDiscountRate.toUpperCase().equals("Y")) {
-				//The boolean variable useCustomRate is used so that the programme knows what value to use when calculating discounts later.
-				useCustomRate = true;
-				//Takes custom discount percentage from user. A check is completed to ensure the number is valid. (Between 0 and 100)
-				while(true) {
-					System.out.println("Enter Custom Discount Rate Percentage: ");
-					customRate = console.nextDouble();
-					if(customRate > 0 && customRate < 100) {
+				while (true) {
+					
+					System.out.println("Specify Custom Discount Rate [Y|N]: ");
+					String customDiscountRate = console.nextLine();
+					
+					// .toUpperCase is used in case the user submits a lower case character.
+					// The OR operator could also be used eg: if (customDiscountRate.equals("Y") || customDiscountRate.equals("y")){}
+					if (customDiscountRate.toUpperCase().equals("Y")) {
+						//The boolean variable useCustomRate is used so that the programme knows what value to use when calculating discounts later.
+						useCustomRate = true;
+						//Takes custom discount percentage from user. A check is completed to ensure the number is valid. (Floating point number Between 0 and 100)
+												
+						while(true) {
+							invalidInput = false;
+							System.out.println("Enter Custom Discount Rate Percentage: ");
+							try {
+								customRate = console.nextDouble();
+							}catch(Exception e){
+								System.out.println("Invalid input" + "( "+e+" )");
+								invalidInput = true;
+								console.next();
+								continue;
+							}
+							if(invalidInput == false) {
+								if(customRate > 0 && customRate < 100) {
+									break;
+								}else {
+									System.out.println("Please enter a number between 0-100.");
+								}
+							}
+						}
+						
+						break;
+					}else if(customDiscountRate.toUpperCase().equals("N")) {
+						useCustomRate = false;
 						break;
 					}else {
-						System.out.println("Please enter a number between 0-100.");
+						System.out.println("Please use only 'Y' or 'N' Characters to proceed.");
+						
 					}
 				}
-				break;
-			}else if(customDiscountRate.toUpperCase().equals("N")) {
-				useCustomRate = false;
-				break;
-			}else {
-				System.out.println("Please use only 'Y' or 'N' Characters to proceed.");
-				
-			}
-		}
+			
 		
 		//Try catch is used to prevent the program from suffering a fatal error should the data file not be present / unreadable		
 		try{
