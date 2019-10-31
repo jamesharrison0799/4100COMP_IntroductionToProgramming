8dcf6935f80fcdf570172c5e40dda4112ac2793d
James Harrison <harrison.james0799@gmail.com>
2019-10-31 19:21:54 +0000

    Tut 11 & SeatDiscount2

---
+++ 4100COMP_Tutorial_11_Collections1/src/arrays/strArray.java
@@ -0,0 +1,19 @@
+package arrays;
+
+public class strArray {
+	public static void main(String[] args) {
+
+		String[] strArray = new String[5];
+
+		strArray[0] = "This";
+		strArray[1] = "is";
+		strArray[2] = "a";
+		strArray[3] = "String";
+		strArray[4] = "Array";
+
+		for (int i = 0; i <= strArray.length; i++) {
+			System.out.print(strArray[i] + " ");
+		}
+
+	}
+}
