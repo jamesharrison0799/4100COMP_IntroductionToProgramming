0e62bb3855127f99f70d0614929aa8a8cb5af1a2
James Harrison <harrison.james0799@gmail.com>
2019-12-07 22:46:10 +0000

    Updated coursework 2

--- 4100COMP_Coursework2/src/coursework2/Coursework2.java
+++ 4100COMP_Coursework2/src/coursework2/Coursework2.java
@@ -1,44 +1,90 @@
 package coursework2;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Coursework2 {
 
 	static Scanner console = new Scanner(System.in);
-
+	static Scanner read;
+	static String email;
+	static final String file = "seats.txt";
+	
 	public static void main(String[] args) {
-
+	
+		onStart();
+	}
+	
+	public static void onStart() {
+		System.out.println("Welcome to 'GENERIC SEAT BOOKING PROGRAM'!\nFor all your seat booking needs.\nTo start,");
+		setEmail();
+		System.out.printf("Welcome %s\n",email);
+		//getDataStats();
 		menu();
 	}
 
+	public static void setEmail() {
+		System.out.println("Please enter your email.\n>");
+		email = console.nextLine(); 	
+		//Check email
+		if(!isValidEmail(email)) {
+			System.out.println("Email Address is invalid.");
+			setEmail();
+		}
+	}
+	
+	public static boolean isValidEmail(String email) {
+		//REGEX from https://www.owasp.org/index.php/OWASP_Validation_Regex_Repository
+		String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+ 
+                "[a-zA-Z0-9_+&*-]+)*@" + 
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" + 
+                "A-Z]{2,10}$"; 
+		Pattern pat = Pattern.compile(emailRegex);
+		if(email == null) {
+			return false;
+		}
+		return pat.matcher(email).matches();
+	}
+	
+	public static void readFile(){
+		//DATA FORMAT: SEATNUM CLASS isWindow isAile isWindow PRICE AVALIABILITY/EMAIL
+		
+		
+	}
+	
+	// ---------------MENU---------------
 	public static void menu() {
-		// clearConsole();
-		System.out.println("- - Seat Booking System - -\n" + "\r\n" + "- - MAIN MENU - -\n" + "1 - Reserve Seat\n"
-				+ "2 - Cancel Seat\n" + "3 - View Seat Reservations\n" + "Q - Quit\n" + "Pick :\n");
+		
+		System.out.printf("- - MAIN MENU - -\n\nCurrent user: %s\n\n1 - Reserve Seat\n"
+				+ "2 - Cancel Seat\n3 - View Seat Reservations\n4 - Change Email\nQ - Quit\nPick :\n",email);
 
 		switch (console.nextLine().toUpperCase()) {
 		case "1":
 			reserveSeat();
+			menu();
 		case "2":
 			cancelSeat();
+			menu();
 		case "3":
 			viewReservation();
+			menu();
 		case "Q":
 			quit();
+		case "4":
+			setEmail();
+			menu();
 		default:
 			menuError("Invalid Selection");
 		}
 	}
 
+	// ---------------Functions---------------
 	public static void reserveSeat() {
-		// System.out.println(readFile(1, null));
-		for (String i : readFile(1, null)) {
-			System.out.println(i);
-		}
 
 	}
 
@@ -54,7 +100,6 @@ public class Coursework2 {
 		System.out.println("The program is about to terminate.\nPress 'y'" + "key to continue, 'n' to return to menu.");
 		switch (console.nextLine()) {
 		case "y":
-			clearConsole();
 			System.exit(0);
 		case "n":
 			menu();
@@ -67,54 +112,4 @@ public class Coursework2 {
 		System.out.println("MenuError\n" + e);
 	}
 
-	public static String getEmail() {
-		String email = console.nextLine();
-		return email;
-
-	}
-
-	// Types of file read:
-	// 1 - get all
-	// 2 - get all available
-	// 3 - get specific
-	public static ArrayList<String> readFile(int type, String email) {
-		try {
-			Scanner read = new Scanner(new FileReader("seats.txt"));
-
-			ArrayList<String> lines = new ArrayList<>();
-			ArrayList<String> availableSeats = new ArrayList<>();
-			while (read.hasNext()) {
-				String line = read.nextLine();
-				lines.add(line);
-			}
-
-			switch (type) {
-			case 1:
-				return lines;
-			case 2:
-				for (String i : lines) {
-					if (i.contains("free")) {
-						availableSeats.add(i);
-					}
-				}
-				return availableSeats;
-			case 3:
-				break;
-			default:
-				break;
-			}
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			menu();
-		}
-
-		return null;
-	}
-
-	public static void clearConsole() {
-		for (int i = 0; i <= 100; i++) {
-			System.out.println("\n");
-		}
-	}
 }
